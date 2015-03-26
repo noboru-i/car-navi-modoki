6c2a039402e2a94c972d2e3b7eb87af2589ea699
noboru-i <ishikura.noboru@gmail.com>
2015-03-26 19:05:04 +0900

    充電の開始・終了を検知

---
+++ app/src/main/java/hm/orz/chaos114/android/carnavimodoki/PowerConnectionReceiver.java
@@ -0,0 +1,20 @@
+package hm.orz.chaos114.android.carnavimodoki;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class PowerConnectionReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        switch (intent.getAction()) {
+            case Intent.ACTION_POWER_CONNECTED:
+                Toast.makeText(context, "接続！" + intent.getExtras(), Toast.LENGTH_LONG).show();
+                break;
+            case Intent.ACTION_POWER_DISCONNECTED:
+                Toast.makeText(context, "切断！" + intent.getExtras(), Toast.LENGTH_LONG).show();
+                break;
+        }
+    }
+}
