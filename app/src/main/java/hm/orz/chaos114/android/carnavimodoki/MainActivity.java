0051388aa53a9b23d0d810056ddcdf0957ec0def
noboru-i <ishikura.noboru@gmail.com>
2015-03-26 19:35:22 +0900

    とりあえず、音楽の一覧を取得

--- app/src/main/java/hm/orz/chaos114/android/carnavimodoki/MainActivity.java
+++ app/src/main/java/hm/orz/chaos114/android/carnavimodoki/MainActivity.java
@@ -1,39 +1,76 @@
 package hm.orz.chaos114.android.carnavimodoki;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.ContentResolver;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class MainActivity extends ActionBarActivity {
+    private static final String ALBUM_ARTIST = "album_artist";
+    private static final String[] COLUMNS = new String[]{
+            MediaStore.Audio.Media._ID,
+            MediaStore.Audio.Media.ARTIST,
+            ALBUM_ARTIST,
+            MediaStore.Audio.Media.ALBUM,
+            MediaStore.Audio.Media.DURATION,
+            MediaStore.Audio.Media.TRACK,
+            MediaStore.Audio.Media.TITLE
+    };
+
+    @InjectView(R.id.text)
+    TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        ButterKnife.inject(this);
+
+        initContent();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void initContent() {
+        ContentResolver cr = getApplicationContext().getContentResolver();
+        Cursor cursor = cr.query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                COLUMNS,
+                null,
+                null,
+                ALBUM_ARTIST + " ASC, " + MediaStore.Audio.Media.ALBUM_ID + " ASC");
+        cursor.moveToFirst();
+
+        while (cursor.moveToNext()) {
+            String title = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE));
+            String artist = cursor.getString(cursor.getColumnIndex(ALBUM_ARTIST));
+            String album = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM));
+            if (artist == null) {
+                artist = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST));
+            }
+            mTextView.append(artist + "/" + album + "/" + title + "\n");
+        }
     }
 }
