06bb9eef27b6b88f92b4b5132e14def050974ecb
noboru-i <ishikura.noboru@gmail.com>
2015-04-03 23:57:55 +0900

    電源接続時にアプリが起動するように実装

--- app/src/main/java/hm/orz/chaos114/android/carnavimodoki/receiver/PowerConnectionReceiver.java
+++ app/src/main/java/hm/orz/chaos114/android/carnavimodoki/receiver/PowerConnectionReceiver.java
@@ -3,18 +3,28 @@ package hm.orz.chaos114.android.carnavimodoki.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.widget.Toast;
+
+import hm.orz.chaos114.android.carnavimodoki.App;
+import hm.orz.chaos114.android.carnavimodoki.activity.MainActivity;
+import hm.orz.chaos114.android.carnavimodoki.service.MusicService;
 
 public class PowerConnectionReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         switch (intent.getAction()) {
             case Intent.ACTION_POWER_CONNECTED:
-                Toast.makeText(context, "接続！" + intent.getExtras(), Toast.LENGTH_LONG).show();
+                startApplication(context);
                 break;
             case Intent.ACTION_POWER_DISCONNECTED:
-                Toast.makeText(context, "切断！" + intent.getExtras(), Toast.LENGTH_LONG).show();
+                // TODO ディスプレイを切る
+                App.Bus().post(MusicService.ControlEvent.PAUSE);
                 break;
         }
+    }
+
+    private void startApplication(Context context) {
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
     }
 }
