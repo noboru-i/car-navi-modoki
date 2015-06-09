ae23a5c5042dcbb920d894e6e8dd3ea06237f570
noboru-i <ishikura.noboru@gmail.com>
2015-06-09 22:41:19 +0900

    fix gem

--- scripts/saddler.sh
+++ scripts/saddler.sh
@@ -2,10 +2,11 @@
 
 NOTIFIER_CONTEXT="saddler/android"
 
+gem install --no-document checkstyle_filter-git saddler saddler-reporter-github github_status_notifier
+
 github-status-notifier notify --state pending --context "${NOTIFIER_CONTEXT}"
 
 ./gradlew check
 
-gem install --no-document checkstyle_filter-git saddler saddler-reporter-github github_status_notifier
 cat app/build/reports/checkstyle/checkstyle.xml | checkstyle_filter-git diff origin/master | saddler report --require saddler/reporter/github --reporter Saddler::Reporter::Github::PullRequestReviewComment
 github-status-notifier notify --exit-status $? --context "${NOTIFIER_CONTEXT}"
