702ee78775d01184e12545e141881a2b10a6b406
noboru-i <ishikura.noboru@gmail.com>
2015-06-09 22:31:04 +0900

    add pending

--- scripts/saddler.sh
+++ scripts/saddler.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+NOTIFIER_CONTEXT="saddler/android"
+
+github-status-notifier notify --state pending --context "${NOTIFIER_CONTEXT}"
+
 ./gradlew check
 
 gem install --no-document checkstyle_filter-git saddler saddler-reporter-github github_status_notifier
 cat app/build/reports/checkstyle/checkstyle.xml | checkstyle_filter-git diff origin/master | saddler report --require saddler/reporter/github --reporter Saddler::Reporter::Github::PullRequestReviewComment
-github-status-notifier notify --exit-status $? --context "saddler/android"
+github-status-notifier notify --exit-status $? --context "${NOTIFIER_CONTEXT}"
