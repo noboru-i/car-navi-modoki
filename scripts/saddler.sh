70138aea9d0e0149db446bba7b8a986316277b1e
noboru-i <ishikura.noboru@gmail.com>
2015-06-11 14:02:23 +0900

    findbugsのレビューを追加

--- scripts/saddler.sh
+++ scripts/saddler.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
-gem install --no-document checkstyle_filter-git saddler saddler-reporter-github
+gem install --no-document checkstyle_filter-git saddler saddler-reporter-github findbugs_translate_checkstyle_format
 
 ./gradlew check
 
+# checkstyle
 cat app/build/reports/checkstyle/checkstyle.xml \
+    | checkstyle_filter-git diff origin/master \
+    | saddler report --require saddler/reporter/github --reporter Saddler::Reporter::Github::PullRequestReviewComment
+
+# findbugs
+cat app/build/reports/findbugs/findbugs.xml \
+    | findbugs_translate_checkstyle_format translate \
     | checkstyle_filter-git diff origin/master \
     | saddler report --require saddler/reporter/github --reporter Saddler::Reporter::Github::PullRequestReviewComment
